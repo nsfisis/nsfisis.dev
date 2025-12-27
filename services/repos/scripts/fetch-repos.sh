14c35e8ac247c205ba21daaef3b58c6ca467d519
nsfisis <nsfisis@gmail.com>
2025-12-28 01:44:25 +0900

    fix(repos): correct updated time

--- services/repos/scripts/fetch-repos.sh
+++ services/repos/scripts/fetch-repos.sh
@@ -7,5 +7,13 @@ for repo_path in repos/*; do
         repo_name=$(basename "$repo_path" .git)
         echo "Fetching $repo_name"
         git -C "$repo_path" fetch --tags --prune origin
+
+        # Update agefile.
+        mkdir -p "$repo_path/info/web"
+        git -C "$repo_path" for-each-ref \
+            --sort=-committerdate \
+            --format='%(committerdate:format:%Y-%m-%d %H:%M:%S)' \
+            refs/ \
+        | head -1 > "$repo_path/info/web/last-modified"
     fi
 done
